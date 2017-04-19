fadcd534f93d3f77d2f424f873d5963da50c3556
Alexander Goida <alexander.goida@gmail.com>
2017-04-20 02:55:37 +0300

    Added [Required]

--- BookStore.Api/Models/StoreCreateModel.cs
+++ BookStore.Api/Models/StoreCreateModel.cs
@@ -1,8 +1,12 @@
-﻿namespace BookStore.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.Models
 {
     public class StoreCreateModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public LocationAddressModel Address { get; set; }
     }
 }
--- BookStore.Api/Models/StoreUpdateModel.cs
+++ BookStore.Api/Models/StoreUpdateModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Api.Models
 {
     public class StoreUpdateModel
     {
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public LocationAddressModel Address { get; set; }
     }
 }
