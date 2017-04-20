14d4e3c1e146051b920b870b04bc10fb9596f688
Alexander Goida <alexander.goida@gmail.com>
2017-04-20 21:13:53 +0300

    Added comment to data model

--- BookStore.Data/Models/Author.cs
+++ BookStore.Data/Models/Author.cs
@@ -2,8 +2,14 @@
 
 namespace BookStore.Data.Models
 {
+    /// <summary>
+    /// Author data model
+    /// </summary>
     public class Author : Entity
     {
+        /// <summary>
+        /// Name
+        /// </summary>
         public string Name { get; set; }
         public IList<Book> Books { get; set; } = new List<Book>();
     }
